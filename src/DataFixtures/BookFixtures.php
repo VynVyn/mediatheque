42c9ff34b8c39c71aeaf78644a4be6da2d6ad25e
pauline-gidon <79903854+pauline-gidon@users.noreply.github.com>
2022-05-31 15:23:06 +0200

    factory model

---
+++ src/DataFixtures/BookFixtures.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\BookFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class BookFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        BookFactory::createMany(50);
+    }
+
+}
