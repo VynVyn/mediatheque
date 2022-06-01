ff457ae69c9931fe53bc0e7672d8a10acdd36342
pauline-gidon <79903854+pauline-gidon@users.noreply.github.com>
2022-06-01 16:29:29 +0200

    pagination liste document

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,21 +2,30 @@
 
 namespace App\Controller;
 
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use App\Repository\BookRepository;
-use App\Repository\DocumentRepository;
 use App\Repository\FilmRepository;
+use App\Repository\DocumentRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(DocumentRepository $documents): Response
+    public function index(DocumentRepository $documents, Request $request): Response
     {
+
+        $querybuilder = $documents->getQueryBuilderForList();
+
+        $pagerfanta = new Pagerfanta(new QueryAdapter($querybuilder));
+        $pagerfanta->setMaxPerPage(10);
+        $pagerfanta->setCurrentPage($request->query->get('page', 1));
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-            'documents' => $documents->findAll(),
+            'pager' => $pagerfanta,
         ]);
     }
 }
--- src/Repository/DocumentRepository.php
+++ src/Repository/DocumentRepository.php
@@ -3,8 +3,9 @@
 namespace App\Repository;
 
 use App\Entity\Document;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Document>
@@ -39,28 +40,9 @@ class DocumentRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Document[] Returns an array of Document objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function getQueryBuilderForList(): QueryBuilder
+    {
+        return $this->createQueryBuilder('d');
+    }
 
-//    public function findOneBySomeField($value): ?Document
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
