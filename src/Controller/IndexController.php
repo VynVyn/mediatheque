47647103aac1823d5164b5f08421f00663907fbd
Vincent Roumy <vincent.roumy@colombbus.org>
2022-06-01 13:04:48 +0200

    juste book

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,19 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use App\Repository\DocumentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(DocumentRepository $documents): Response
+    public function index(BookRepository $books): Response
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
-            'documents' => $documents->findAll(),
+            'documents' => $books->findAll(),
         ]);
     }
 }
