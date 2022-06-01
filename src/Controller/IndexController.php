2d53494c508c5d57f00e7bf2579a4f1019ce9e25
Vincent Roumy <vincent.roumy@colombbus.org>
2022-06-01 15:05:51 +0200

    twig extension

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\BookRepository;
 use App\Repository\DocumentRepository;
+use App\Repository\FilmRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,11 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(BookRepository $books): Response
+    public function index(DocumentRepository $documents): Response
     {
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
-            'documents' => $books->findAll(),
+            'documents' => $documents->findAll(),
         ]);
     }
 }
