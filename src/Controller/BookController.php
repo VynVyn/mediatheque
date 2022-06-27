b8fac50955be7a31fffdc9a3c586370a9a7595db
Vincent Roumy <vincent.roumy@colombbus.org>
2022-06-27 12:57:57 +0200

    book/film route definition

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -10,10 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/book')]
+#[Route('/admin/book', name: 'book_')]
 class BookController extends AbstractController
 {
-    #[Route('/', name: 'app_book_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(BookRepository $bookRepository): Response
     {
         return $this->render('book/index.html.twig', [
@@ -21,7 +21,7 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_book_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, BookRepository $bookRepository): Response
     {
         $book = new Book();
@@ -31,7 +31,7 @@ class BookController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $bookRepository->add($book, true);
 
-            return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('book/new.html.twig', [
@@ -40,7 +40,7 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Book $book): Response
     {
         return $this->render('book/show.html.twig', [
@@ -48,7 +48,7 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_book_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Book $book, BookRepository $bookRepository): Response
     {
         $form = $this->createForm(BookType::class, $book);
@@ -57,7 +57,7 @@ class BookController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $bookRepository->add($book, true);
 
-            return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('book/edit.html.twig', [
@@ -66,13 +66,13 @@ class BookController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_book_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Book $book, BookRepository $bookRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->request->get('_token'))) {
             $bookRepository->remove($book, true);
         }
 
-        return $this->redirectToRoute('app_book_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('book_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -10,10 +10,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/film')]
+#[Route('/admin/film', name: 'film_')]
 class FilmController extends AbstractController
 {
-    #[Route('/', name: 'app_film_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(FilmRepository $filmRepository): Response
     {
         return $this->render('film/index.html.twig', [
@@ -21,7 +21,7 @@ class FilmController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_film_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, FilmRepository $filmRepository): Response
     {
         $film = new Film();
@@ -31,7 +31,7 @@ class FilmController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $filmRepository->add($film, true);
 
-            return $this->redirectToRoute('app_film_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('film_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('film/new.html.twig', [
@@ -40,7 +40,7 @@ class FilmController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_film_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Film $film): Response
     {
         return $this->render('film/show.html.twig', [
@@ -48,7 +48,7 @@ class FilmController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_film_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Film $film, FilmRepository $filmRepository): Response
     {
         $form = $this->createForm(FilmType::class, $film);
@@ -57,7 +57,7 @@ class FilmController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $filmRepository->add($film, true);
 
-            return $this->redirectToRoute('app_film_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('film_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('film/edit.html.twig', [
@@ -66,13 +66,13 @@ class FilmController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_film_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Film $film, FilmRepository $filmRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$film->getId(), $request->request->get('_token'))) {
             $filmRepository->remove($film, true);
         }
 
-        return $this->redirectToRoute('app_film_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('film_index', [], Response::HTTP_SEE_OTHER);
     }
 }
